00ca6d3dd4da93f7223482df8e26d877138afecd
cameronfr <catechboy@hotmail.com>
2020-04-13 16:48:53 +0900

    market contract v1

---
+++ onchain/myERC721Marketplace.sol
@@ -0,0 +1,74 @@
+pragma solidity ^0.5.0;
+
+import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/release-v2.5.0/contracts/token/ERC721/IERC721.sol";
+import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-contracts/release-v2.5.0/contracts/math/SafeMath.sol";
+
+
+contract Market {
+
+  using SafeMath for uint256;
+
+  // Token contract
+  address private constant tokenContractAddress = 0xcEEF34aa024F024a872b4bA7216e9741Ac011efe;
+  IERC721 private constant tokenContract = IERC721(tokenContractAddress);
+
+  // State
+  mapping (uint256 => address) private tokenIsForSale; // address is the owner who listed it
+  mapping (uint256 => uint256) private tokenPrice;
+
+  // Fee
+  uint256 private feePower = 5; // 1.0/(2^5) = 0.03125 = ~3%
+  address payable private feeRecipient = 0xf91B98fe5Cc2a590C5d3DA72324f8A52F241D96B;
+
+  constructor () public {
+  }
+
+  function list(uint256 tokenId, uint256 price) external {
+    address tokenOwner = tokenContract.ownerOf(tokenId);
+    require(msg.sender == tokenOwner, "Invalid access");
+
+    tokenIsForSale[tokenId] = tokenOwner;
+    tokenPrice[tokenId] = price;
+  }
+
+  function delist(uint256 tokenId) external {
+    address tokenOwner = tokenContract.ownerOf(tokenId);
+    require(msg.sender == tokenOwner, "Invalid access");
+
+    tokenIsForSale[tokenId] = address(0);
+  }
+
+  function buyFungible(uint256 tokenId) payable external {
+    address tokenOwner = tokenContract.ownerOf(tokenId);
+    uint256 price = tokenPrice[tokenId];
+
+    require(tokenIsForSale[tokenId] != address(0));
+    require(tokenIsForSale[tokenId] == tokenOwner); // current owner must be the owner who listed it
+    require(msg.value == price);
+
+    uint256 fee = msg.value >> feePower;
+    uint256 payment = msg.value - fee;
+    address payable tokenOwnerPayable = address(uint160(tokenOwner)); // not nice
+
+    tokenIsForSale[tokenId] = address(0);
+    tokenContract.transferFrom(tokenOwner, msg.sender, tokenId);
+    feeRecipient.transfer(fee);
+    tokenOwnerPayable.transfer(payment); // untrusted
+  }
+
+  // Public view functions
+
+  function isListed(uint256 tokenId) view public returns (bool) {
+    address tokenOwner = tokenContract.ownerOf(tokenId);
+    bool listingExists = tokenIsForSale[tokenId] != address(0);
+    bool listingValid = tokenIsForSale[tokenId] == tokenOwner;
+
+    return (listingExists && listingValid);
+  }
+
+  function listingPrice(uint256 tokenId) view public returns (uint256) {
+    require(isListed(tokenId));
+    uint256 price = tokenPrice[tokenId];
+    return price;
+  }
+}
